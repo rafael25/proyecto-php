199edf589d42b35ba697ea8b8f1cf2f704ce4014
rafael25 <rafaelvb25@gmail.com>
2015-01-23 17:54:41 -0600

    Registra auto cargador en index

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @Author: rafael
+ * @Date:   2015-01-23 16:04:40
+ * @Last Modified by:   Administrador
+ * @Last Modified time: 2015-01-23 17:54:13
+ */
+
+/**
+ * Se registra el cargador de clases
+ */
+include __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'cargador.php';
+
+
+?>
