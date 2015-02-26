a6d62b07c5c5045b27715a21b78c89c845b10fd7
rafael25 <rafaelvb25@gmail.com>
2015-02-25 18:04:08 -0600

    Agrega conexion a base de datos

--- src/App/servicios.php
+++ src/App/servicios.php
@@ -2,8 +2,8 @@
 /**
  * @Author: rafael25
  * @Date:   2015-02-25 00:56:25
- * @Last Modified by:   rafael25
- * @Last Modified time: 2015-02-25 01:06:39
+ * @Last Modified by:   Administrador
+ * @Last Modified time: 2015-02-25 18:03:46
  */
 
 $contenedor = new \Base\Micro\Contenedor;
@@ -11,6 +11,15 @@ $contenedor = new \Base\Micro\Contenedor;
 $contenedor->set('router', function() {
 	$router = include __DIR__ . DIRECTORY_SEPARATOR . 'rutas.php';
 	return $router;
+});
+
+$contenedor->set('db', function () {
+	try {
+		$conn = new PDO("mysql:dbname=recetas;host=127.0.0.1", "root", "");
+	} catch (PDOException $e) {
+		echo "Fallo de conexion" . $e->getMessage();
+	}
+	return $conn;
 });
 
 return $contenedor;
