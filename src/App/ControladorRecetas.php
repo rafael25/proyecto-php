825b97f6440d826c197598f2b9a2df7df7331c6a
rafael25 <rafaelvb25@gmail.com>
2015-03-04 04:20:57 -0600

    Corrige método buscarID

--- src/App/ControladorRecetas.php
+++ src/App/ControladorRecetas.php
@@ -14,13 +14,22 @@ class ControladorRecetas extends ControladorBase {
 		$recetas = $this->db->query("SELECT * FROM recetas");
 		$vista = new Vista("index.html");
 		$vista->recetas = $recetas;
+		$vista->docTitle = 'Lista de Recetas';
 		echo $vista;
 	}
 
+	/**
+	 * @ruta '/recetas/id'
+	 * @metodo 'get'
+	 * @param  int id
+	 * @return Vista
+	 */
 	public function buscarId($id) {
 		$receta = $this->db->query("SELECT * FROM recetas where id=$id");
 		$vista = new Vista("receta.html");
+		$receta = $receta->fetch();
 		$vista->receta = $receta;
+		$vista->docTitle = 'Receta: ' . $receta['nombre'];
 		echo $vista;
 	}
 }
