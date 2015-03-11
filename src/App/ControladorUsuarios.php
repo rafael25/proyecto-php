832704830708fc06e5dd600e888e8e7dc5b1256b
rafael25 <rafaelvb25@gmail.com>
2015-03-11 17:54:50 -0600

    Registra rutas para manejo de sesiones de usuario

---
+++ src/App/ControladorUsuarios.php
@@ -0,0 +1,28 @@
+<?php namespace App;
+/**
+ * @Author: rafael
+ * @Date:   2015-03-11 17:45:23
+ * @Last Modified by:   Administrador
+ * @Last Modified time: 2015-03-11 17:50:09
+ */
+
+use \Base\Micro\ControladorBase;
+
+class ControladorUsuarios extends ControladorBase {
+
+	public function loginForm() {
+		// TODO: Crear la vista de formulario de login
+	}
+
+	public function login() {
+		// TODO: Implementar el inicio de sesiones
+	}
+
+	public function signInForm() {
+		// TODO: Crear la vista de formulario de registro
+	}
+
+	public function signIn() {
+		// TODO: Implementar el registro de usuarios
+	}
+}
--- src/App/rutas.php
+++ src/App/rutas.php
@@ -3,20 +3,30 @@
  * @Author: rafael25
  * @Date:   2015-02-20 22:43:44
  * @Last Modified by:   Administrador
- * @Last Modified time: 2015-03-10 18:49:19
+ * @Last Modified time: 2015-03-11 17:51:50
  */
 
 use Base\Micro\Router;
 
 $router = new Router;
 
+/*--------- Recetas ---------*/
 $router->agregar('get', '/', 'App\ControladorRecetas::index');
 $router->agregar('get', '/recetas', 'App\ControladorRecetas::index');
 $router->agregar('get', '/recetas/(\d+)', 'App\ControladorRecetas::buscarId');
 $router->agregar('get', '/recetas-nueva', 'App\ControladorRecetas::ingresar');
 $router->agregar('post', '/recetas', 'App\ControladorRecetas::guardar');
+
+/*--------- Comentarios ---------*/
 $router->agregar('post', '/comentarios', 'App\ControladorComentarios::guardar');
 
+/*--------- Usuarios ---------*/
+$router->agregar('get', '/login', 'App\ControladorUsuarios::loginForm');
+$router->agregar('post', '/login', 'App\ControladorUsuarios::login');
+$router->agregar('get', '/signin', 'App\ControladorUsuarios::signInForm');
+$router->agregar('post', '/signin', 'App\ControladorUsuarios::signIn');
+
+/*--------- Errores ---------*/
 $router->setRuta404('App\ControladorErrores::error404');
 
 return $router;
