502cf3c7d22264dd88e53162c54142a058fe968f
Rafael Viveros <rafaelvb25@gmail.com>
2015-03-28 19:55:33 -0600

    Ingresar un comentario ahora toma el id de usuario de la sesión

--- src/App/ControladorComentarios.php
+++ src/App/ControladorComentarios.php
@@ -2,8 +2,8 @@
 /**
  * @Author: Harumi
  * @Date:   2015-03-10 18:49:51
- * @Last Modified by:   Administrador
- * @Last Modified time: 2015-03-10 19:28:35
+ * @Last Modified by:   rafael
+ * @Last Modified time: 2015-03-26 21:30:09
  */
 
 use \Base\Micro\ControladorBase;
@@ -19,7 +19,7 @@ class ControladorComentarios extends ControladorBase {
 	{
 		$recetaId = $_POST["recetaId"];
 		$comentario =$_POST["comentario"];
-		$usuarioId = 1;
+		$usuarioId = $_SESSION['usuario']['id'];
 
 		$comentario = $this->db->query("INSERT INTO comentarios(receta_id, usuario_id, comentario) VALUES ($recetaId, $usuarioId, '$comentario')");
 
