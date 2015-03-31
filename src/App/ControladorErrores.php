6cea8dcee6111f8139c4c3e0df1bccf84e34a5b5
Rafael Viveros <rafaelvb25@gmail.com>
2015-03-31 13:37:06 -0600

    Corrige error de manejo de error 404

--- src/App/ControladorErrores.php
+++ src/App/ControladorErrores.php
@@ -10,6 +10,7 @@ class ControladorErrores extends ControladorBase {
 	 * Manejador del error 404 'not found'
 	 */
 	public function error404() {
+		header('HTTP/1.0 404 Not Found');
 		$vista = new Vista("error404.html");
 		echo $vista;
 	}
