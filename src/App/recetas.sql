e911c98af496ea12f948cfcecdd2fe47e0a00508
Rafael Viveros <rafaelvb25@gmail.com>
2015-03-31 13:37:48 -0600

    Agrega esquema de base de datos

---
+++ src/App/recetas.sql
@@ -0,0 +1,118 @@
+INSERT INTO recetas.recetas (id, nombre, descripcion, ingredientes, preparacion, tiempo_prep, rendimiento) VALUES (1, 'Berenjenas al ajillo y vino blanco', 'Ensaladas y vegetales', 'Ingredientes
+3 berenjenas
+Sal de grano
+4 ajos picados
+5 chiles guajillos
+½ taza de vino blanco', 'Cortar las berenjenas en rodajas de aproximadamente un dedo de ancho, espolvorear sal de grano y acomodar para que suden y suelten lo amargo. Precalentar una sartén y sofreír el ajo hasta que empiece a tomar un color dorado. Cortar el guajillo en aros y agregar a la sartén. Agregar las berenjenas y cuando empiece a soltar su aroma agregar el vino blanco hasta que se reduzca por competo. Servir. Las podemos acompañar de algún puré o ensalada.', 30, '4 porciones');
+INSERT INTO recetas.recetas (id, nombre, descripcion, ingredientes, preparacion, tiempo_prep, rendimiento) VALUES (2, 'Berros con elotitos asados ', 'Ensaladas y vegetales', '2 tazas de berros
+2 rebanadas de tocino frito
+½ cucharadita de vinagre de vino blanco
+2 cucharaditas de vinagre de jerez
+4 elotitos asados
+1 limón
+Sal al gusto
+1 diente de ajo picado fino
+1 cucharada de cacahuates', 'Tip: Desinfectar 2 tazas de berros en 3 tazas de agua con una gota de cloro o yodo, por 10 minutos.
+Cortar a la mitad 4 elotitos y sazonar con limón y sal al gusto, para después llevar al grill. Para la vinagreta, verter 2 cucharaditas de vinagre de jerez, 1 cucharadita de vinagre de vino blanco, unas gotas de limón, 1 diente de ajo picado muy fino, sal y pimienta al gusto; batir y agregar poco a poco aceite de oliva (para emulsionar). Secar los berros con papel absorbente y dar algunos golpecitos. Tomarlos en racimos pequeños y montar en un plato; agregar cacahuates alrededor de los berros, colocar 2 rebanadas de tocino en trozos, poner los elotitos y verter vinagreta al gusto.', 40, '1 porcion');
+INSERT INTO recetas.recetas (id, nombre, descripcion, ingredientes, preparacion, tiempo_prep, rendimiento) VALUES (3, 'Banderillas de pollo ', 'Aves', '80 g de pechuga de pollo en cubos
+¼ de pimiento rojo en cuadros
+¼ de pimiento verde en cuadros
+1 cebolla en cubos
+¼ de taza de piña en cubos
+1 cucharada de miel de abeja
+1 cucharada de salsa de soya
+1 cucharada de jengibre picado', 'Acomodar en palillos largos el pollo, pimientos, piña y cebolla, untar mezclando con soya, miel de abeja, jengibre, sal y pimienta.
+', 15, '2 porciones');
+INSERT INTO recetas.recetas (id, nombre, descripcion, ingredientes, preparacion, tiempo_prep, rendimiento) VALUES (15, 'Crema de betabel', null, '1 litro de caldo de pollo
+6 betabeles cocidos
+4 piezas de pan integral para adornar
+1 cucharada de crema
+Sal y pimienta', 'Licuar el betabel pelado y cocido con caldo de pollo, añadir a una olla y agregar un poco más de caldo de pollo, dejar a fuego lento con sal y pimienta al gusto. Al servir acompañamos con pan integral tostado y un poco de crema.
+', 25, '4 porciones');
+INSERT INTO recetas.recetas (id, nombre, descripcion, ingredientes, preparacion, tiempo_prep, rendimiento) VALUES (16, 'Crema cubana de frijol', null, '1 1/2 taza de frijoles negros secos
+3 tazas de agua
+2 dientes de ajo
+1 cebolla cortada en cuatro
+1 pimiento verde cortado en tres
+4 cucharadas de aceite de oliva
+1 hoja de laurel
+1/4 cucharadita de comino
+Sal y pimienta al gusto
+1/4 taza de vino blanco o tinto seco
+1/2 taza de crema de leche
+Crutones de pan tostado al gusto', 'Enjuagar bien los frijoles en agua fría y ponerlos en la olla express, añadir agua y dejar remojar por lo menos ocho horas. Pasado ese tiempo ponerlos a cocer a fuego alto en la olla junto con 1/3 del pimiento verde y la hoja de laurel. Cerrar la olla herméticamente, colocar el regulador sobre la válvula de escape hasta que comience a silbar, en ese momento reducir el fuego a moderado, manteniendo la presión alta, y cocinar durante 20 minutos más o hasta que los frijoles estén suaves. Retirar la hoja de laurel, poner en la licuadora los frijoles, pimiento, cebolla y suficiente líquido de los frijoles y licuarlos. En una sartén caliente freír con aceite de oliva el ajo machacado y un poco de comino, retirar del fuego y añadir los frijoles licuados, el comino, la sal y la pimienta. Después regresar todo esto a la olla y cocinar a fuego bajo por unos 15 minutos. Para que se mezclen los sabores incorpore el vino, deje dos minutos más y retire del fuego. Puede servirlos con un cucharada de crema, crutones de pan o queso rallado.', 35, '1 porcion');
+INSERT INTO recetas.recetas (id, nombre, descripcion, ingredientes, preparacion, tiempo_prep, rendimiento) VALUES (17, 'Torrejas especiales', null, '1 baguette del día anterior o 1 pan de caja
+3 yemas
+1 lata de leche evaporada
+1 taza azúcar blanca
+1/4 taza de jerez
+1 cucharadita de vainilla
+1 cucharadita de canela
+4 huevos
+1/2 taza de aceite
+
+Almíbar
+1 taza de azúcar
+1 taza de agua
+1/2 cucharadita de vainilla
+1 rajita de canela
+1 cascarita de limón', 'Mezclar el azúcar con el agua, la cascarita de limón y la rajita de canela, poner a hervir hasta que tenga la consistencia de punto de hebra, dejar enfriar y refrigerar. Batir las yemas, añadir la leche evaporada, el azúcar, el jerez, la vainilla y la canela en polvo, remojar las rebanadas de pan en esta mezcla por unos minutos, pasarlas por huevo batido y freírlas en aceite. Servir las torrejas frías con el almíbar.', 35, '1 porcion');
+INSERT INTO recetas.recetas (id, nombre, descripcion, ingredientes, preparacion, tiempo_prep, rendimiento) VALUES (18, 'Arroz con pollo', null, '3 dientes de ajo
+1 cucharada de sal
+1 cucharadita de pimienta
+1/4 taza de jugo de naranja agria
+1 kg de muslos de pollo, pechuga y pierna
+1/4 de aceite vegetal
+2 cebollas medianas
+1 pimiento verde
+3 tazas de caldo de pollo
+4 hilos de azafrán tostados en seco en una sartén a fuego moderado, durante 30 seg.
+2 cucharadas de salsa de tomate
+2 tazas de arroz de Valencia
+Vino
+2 latas pequeñas de chíncharos, escurridos y cortados en tiras.', 'Aplastar los ajos y hacer una pasta, agregar sal y pimienta al gusto, añadir el jugo de naranja y verter este adobo sobre las piezas de pollo. Refrigerar menos de una hora. Mientras tanto, calentar el aceite sobre fuego moderado en una paellera o sartén grande, sacar el pollo del refrigerador y poner sobre el papel absorbente, para después dorarlas en el aceite caliente; reservar el adobo. Apartar las piezas de pollo doradas en el mismo aceite, sofreír la cebolla y el pimiento verde, hasta que las cebollas se tornen translúcidas, añadir el caldo, azafrán, salsa de tomate, adobo reservado y pollo. Cocinar todo a fuego lento unos 5 minutos, añadir el arroz y revolver sólo lo suficiente para cubrir con el líquido. Si el arroz no está cubierto en su totalidad, añadir más caldo, vino o cerveza. Reducir el fuego a moderado y cocinar destapado, hasta que el líquido se haya absorbido y el arroz esté cocido, unos 30 minutos. Añadir más caldo, vino o cerveza. De ser necesario retirar la pollera del fuego, mientras, calentar los chíncharos y pimientos por separado, adorne el arroz con ellos y sirva.', 45, '1 porcion');
+INSERT INTO recetas.recetas (id, nombre, descripcion, ingredientes, preparacion, tiempo_prep, rendimiento) VALUES (19, 'Arroz con leche', null, '½ taza de harina de arroz
+½ litro de leche entera
+4 cucharadas soperas de leche evaporada
+1 cucharada de cardamomo verde, en polvo
+Almendra fileteada al gusto
+1 cucharada de pistache picado
+1 cucharada de cardamomo entero
+¼ de taza de azúcar', 'Disolvemos sin dejar grumos la harina de arroz en la leche entera fría y la colocamos en un recipiente previamente caliente. La dejamos en el fuego hasta que se reduzca un poco, aproximadamente un 20 por ciento. Debemos mover constantemente la mezcla para que no se queme. Posteriormente agregamos la leche evaporada, el cardamomo, la almendra fileteada y el azúcar al gusto, dejamos coser de siete a diez minutos sin dejar de mover, y listo. Servimos y decoramos con el pistache picado.', 25, '1 porcion');
+INSERT INTO recetas.recetas (id, nombre, descripcion, ingredientes, preparacion, tiempo_prep, rendimiento) VALUES (20, 'Banderillas de fruta con chocolate', null, '1 taza de fresas
+1 kiwi
+4 barras de chocolate
+1 taza de piña picada
+1 taza de zarzamoras
+1 pedazo de sandía
+Bombones
+Palitos de bambú', 'Cortar en trozos la piña, el kiwi, las fresas y la sandía. En un palito de bambú insertar la fresa, zarzamora, bombón, la sandía, el kiwi y la piña. En un plato montar un pedazo de sandía y encima colocar tres banderillas de frutas, para acompañar a lado poner una tacita de chocolate.
+Salsa de chocolate: Sólo dejamos hervir agua y chocolate de barra en una olla. Quitar del fuego cuando el agua comience a espesar y tenga consistencia de chocolate líquido.
+Es muy importante desinfectar las fresas con una gota de cloro o una gota de yodo por 15 minutos. No hay que cortarlas las zarzamoras ni los bombones.', 15, '4 porciones');
+INSERT INTO recetas.recetas (id, nombre, descripcion, ingredientes, preparacion, tiempo_prep, rendimiento) VALUES (21, 'Banana split', null, '½ plátano
+1 cucharada de cada sabor
+1 cucharada de salsa de chocolate
+1 cucharada de nuez picada', 'Acomodar los plátanos por la mitad, en el centro poner los helados, bañar con chocolate, espolvorear con nuez, adornar con chantilly y cereza.
+', 5, '1 porcion');
+INSERT INTO recetas.recetas (id, nombre, descripcion, ingredientes, preparacion, tiempo_prep, rendimiento) VALUES (22, 'Ensalada de mango, con aderezo de coco y chile', null, '1 mango petacón
+½ jícama chica
+2 tazas de espinacas en trozos
+Amaranto tostado', 'Pelar y picar en cubos el mango y la jícama, mezclarlos en una ensaladera, junto con las espinacas, espolvorear el amaranto y agregar el aderezo.
+
+Para el aderezo: Derretir el piloncillo con el vinagre y el aceite de coco. Emulsionar la mezcla anterior con aceite de oliva y el chile.', 40, '4 porciones');
+INSERT INTO recetas.recetas (id, nombre, descripcion, ingredientes, preparacion, tiempo_prep, rendimiento) VALUES (23, 'Turcos (dulce de garbanzo)', null, '2 tazas de garbanzo cocido
+100 g de piloncillo
+¾ de taza de agua
+Clavo al gusto
+1 raja de canela
+1 huevo
+3 yemas
+Hojas de hierbabuena
+Flores', 'Cocer el garbanzo con agua, escurrir, dejarlo enfriar y licuarlo. Disolver el piloncillo en el agua caliente hasta obtener un jarabe espeso, dejarlo enfriar. Agregar las especias molidas e integrar muy bien. Incorporar el huevo y posteriormente las yemas, vaciar la mezcla en un molde de pastel o en moldes individuales y hornear a 180°C por 25 minutos. Retirar del horno, dejar enfriar y desmoldar. Sirve y presenta con hojas de hierbabuena y flores.', 45, '4 porciones');
+INSERT INTO recetas.recetas (id, nombre, descripcion, ingredientes, preparacion, tiempo_prep, rendimiento) VALUES (24, 'Espagueti con salsa de champiñones', null, '400 g de espagueti cocido
+1 ramito de perejil picado
+100 g de requesón
+½ cebolla finamente picada
+1 diente de ajo finamente picado
+1 lata de champiñones
+Sal y pimienta', 'Para cocer el espagueti, poner a hervir agua suficiente para la pasta, cuando ya esté hirviendo ponemos un poco de sal y agregamos la pasta. Mientras se cocina, en una sartén previamente caliente ponemos aceite para sofreír primero la cebolla y después el ajo, apartar. Licuar los champiñones y el requesón para hacer una crema, y agregar esta mezcla a la sartén para que se mezcle con la cebolla y el ajo. Añadimos a la crema el perejil picado, pimienta y el espagueti ya cocido, y mezclamos todo hasta que tome consistencia.', 40, '4 porciones');
