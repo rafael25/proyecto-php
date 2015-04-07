734994d57d7ed60f2c6c5313047fb4f125b558d5
Rafael Viveros <rafaelvb25@gmail.com>
2015-04-07 00:47:26 -0500

    Agrega re-dirección al enviar un comentario

--- src/App/ControladorComentarios.php
+++ src/App/ControladorComentarios.php
@@ -2,8 +2,8 @@
 /**
  * @Author: Harumi
  * @Date:   2015-03-10 18:49:51
- * @Last Modified by:   rafael
- * @Last Modified time: 2015-03-26 21:30:09
+ * @Last Modified by:   Rafael Viveros
+ * @Last Modified time: 2015-04-07 00:32:26
  */
 
 use \Base\Micro\ControladorBase;
@@ -23,5 +23,6 @@ class ControladorComentarios extends ControladorBase {
 
 		$comentario = $this->db->query("INSERT INTO comentarios(receta_id, usuario_id, comentario) VALUES ($recetaId, $usuarioId, '$comentario')");
 
+		$this->router->redireccionA('/recetas/'.$recetaId);
 	}
 }
