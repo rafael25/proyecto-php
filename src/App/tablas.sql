b2da398d0f0284d718121fd100e384f2570db152
Rafael Viveros <rafaelvb25@gmail.com>
2015-04-06 20:38:10 -0500

    Agrega constraints a las tablas

--- src/App/tablas.sql
+++ src/App/tablas.sql
@@ -2,27 +2,27 @@ USE recetas;
 
 CREATE TABLE recetas (
 	id				int PRIMARY KEY auto_increment,
-	nombre			varchar(100),
+	nombre			varchar(100) NOT NULL,
 	ingredientes	text,
 	preparacion		text,
 	rendimiento		varchar(50),
 	tiempo_prep		varchar(50),
 	imagen			varchar(200),
-	autor_id		int
+	autor_id		int NOT NULL
 );
 
 CREATE TABLE usuarios (
 	id			int PRIMARY KEY auto_increment,
 	nombre		varchar(50),
 	apellidos	varchar(100),
-	email		varchar(100),
+	email		varchar(100) NOT NULL UNIQUE,
 	imagen		varchar(200),
-	password	varchar(255)
+	password	varchar(255) NOT NULL
 );
 
 CREATE TABLE comentarios (
 	id 			int PRIMARY KEY auto_increment,
-	receta_id	int,
-	usuario_id	int,
+	receta_id	int NOT NULL,
+	usuario_id	int NOT NULL,
 	comentario	text
 );
